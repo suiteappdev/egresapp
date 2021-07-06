42e771143bd6f73bc15213bf203a292f3b4cb680
Andrew Castro9 <listerine1989@gmail.com>
2021-07-06 17:44:34 -0500

    latest

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -2,5 +2,5 @@ angular
     .module('homer')
     .constant('constants', {
        // apiHost : "http://190.157.105.92:1340/"
-        apiHost : "http://190.157.105.92:1337/"
+        apiHost : "http://209.126.11.89:1337/"
     });
